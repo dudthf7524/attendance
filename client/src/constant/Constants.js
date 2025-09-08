2c7e3d2d77ecc7b87e3c81df6a96205086d18e70
user <dudthf7524@naver.com>
2025-09-08 12:43:35 +0900

    update

--- client/src/constant/Constants.js
+++ client/src/constant/Constants.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:3070";
-// export const API_URL = "http://49.50.135.16:3070";
+// export const API_URL = "http://localhost:3070";
+export const API_URL = "http://49.50.135.16:3070";
